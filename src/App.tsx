98802c045b036ca8fa1b756746f5c489379a18f7
Mariana Kaori <mariana.kaori@usp.br>
2022-12-26 11:53:19 -0300

    configuração de ambiente e logo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import logoGithub from './assets/logo-github.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logoGithub} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
