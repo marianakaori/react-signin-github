b1345137d77a9dec020c7571b04a727e1388617b
Mariana Kaori <mariana.kaori@usp.br>
2023-01-12 16:47:29 -0300

    forgot password incluido no formulario

--- src/components/formulario.tsx
+++ src/components/formulario.tsx
@@ -7,8 +7,11 @@ function FormularioSignIn() {
         <form className={styles.formulario}>
             <label htmlFor="username">Username or email address</label>
             <input type="text" id="username" name="username"/>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password"/>
+            <div className={styles.relative}>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password"/>
+                <a href=" " className={styles.forgotPassword}>Forgot password?</a>
+            </div>
             <input type="submit" id={styles.signInButton} name="signin" value={"Sign in"}/>
         </form>
     </div>
